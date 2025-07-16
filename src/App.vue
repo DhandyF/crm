2fa426ddedc75f98914ea2d6e5e03fed3ed5378c
Moch Dhandy Friyadi <dhandy@orderonline.id>
2025-07-16 19:49:10 +0700

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
   <div id="app">
+    <NavbarView />
     <router-view/>
   </div>
 </template>
+
+<script>
+import NavbarView from '@/components/NavbarView.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavbarView,
+  },
+}
+</script>
---
+++ src/components/NavbarView.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
+        <div class="container-fluid container">
+            <a class="navbar-brand fw-bold" href="/">CRM</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contacts">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/call-logs">Call Logs</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
